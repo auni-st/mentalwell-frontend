23614f97612a28a4f578ea56b1c9cdcca9c3688d
Rahmanhakim7 <hakimrahman@students.amikom.ac.id>
2023-12-04 11:05:02 +0700

    update articel bug

--- src/scripts/views/pages/searcharticel.js
+++ src/scripts/views/pages/searcharticel.js
@@ -1,42 +1,3 @@
-// const searchForm = document.getElementById('searchForm');
-// const searchInput = document.getElementById('searchInput');
-// const contentArticle = document.getElementById('content-articel')
-
-// searchForm.addEventListener('submit', (event) => {
-//   event.preventDefault();
-
-//   const searchTerm = searchInput.value.trim();
-
-//   if (searchTerm !== '') {
-//     const apiUrl = `https://mentalwell-backend.vercel.app/articles?title=${encodeURIComponent(searchTerm)}`;
-
-//     fetch(apiUrl)
-//       .then(response => response.json())
-//       .then(data => {
-//         contentArticle.innerHTML = '';
-
-//         data.forEach((articleData) => {
-//           const articleElement = document.createElement('article');
-
-//           articleElement.innerHTML = `
-//                     <div class="image-articel">
-//                     <img src="${articleData.image}" alt="articel">
-//                     </div>
-//                         <h2>${articleData.title}</h2>
-//                         <p>${articleData.content}</p>
-//                         <div class="button-articel">
-//                         <button type="button" onclick="redirectToDetail()"> Baca Selengkapnya</button>
-//                     </div>
-//                 `;
-//           contentArticle.appendChild(articleElement);
-//         });
-//       })
-//       .catch(error => {
-//         console.error('Error fetching search results:', error)
-//       })
-//   }
-// })
-
 const searchForm = document.getElementById('searchForm');
 const searchInput = document.getElementById('searchInput');
 const contentArticle = document.getElementById('content-articel');
@@ -79,9 +40,6 @@ searchForm.addEventListener('submit', (event) => {
         console.error('Error fetching search results:', error);
       });
   } else {
-    // Jika input pencarian kosong, tampilkan semua konten
-    // Implementasikan kembali logika untuk menampilkan semua konten
-    // Sama seperti ketika halaman pertama dimuat
     fetch('https://mentalwell-backend.vercel.app/articles')
       .then((response) => response.json())
       .then((data) => {
@@ -91,14 +49,18 @@ searchForm.addEventListener('submit', (event) => {
           const articleElement = document.createElement('article');
 
           articleElement.innerHTML = `
-            <div class="image-articel">
-              <img src="${articleData.image}" alt="articel">
-            </div>
-            <h2>${articleData.title}</h2>
-            <p>${articleData.content}</p>
-            <div class="button-articel">
-              <button type="button" onclick="redirectToDetail()"> Baca Selengkapnya</button>
-            </div>
+          <div class="image-articel">
+          <img src="${articleData.image}" alt="articel">
+          </div>
+          <div class="isi-articel">
+              <h2>${articleData.title}</h2>
+              <div class="content"> 
+                <p id="contentParagraph">${articleData.content}</p>
+              </div>
+              <div class="button-articel">
+              <button type="button" onclick="redirectToDetail('${articleData.id}')"> Baca Selengkapnya</button>
+              </div>
+          </div>
           `;
           contentArticle.appendChild(articleElement);
         });
