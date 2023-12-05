dc930f7ae52faf1aca522e2ec9c3a25df16d7489
Gibran Alfarabi <gibranalfarabi18@gmail.com>
2023-12-05 15:20:56 +0700

    update

--- src/scripts/views/pages/riwayat.js
+++ src/scripts/views/pages/riwayat.js
@@ -1,4 +1,4 @@
-const authToken = sessionStorage.getItem('authToken')
+const authToken = sessionStorage.getItem('authToken');
 const containerRiwayat = document.querySelector('.content-riwayat');
 
 fetch('https://mentalwell-backend.vercel.app/history', {
@@ -8,31 +8,48 @@ fetch('https://mentalwell-backend.vercel.app/history', {
     'Content-Type': 'application/json'
   }
 })
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(riwayat => {
-      const riwayatElement = document.createElement('div');
-      riwayatElement.classList.add('container-riwayat');
-
-      riwayatElement.innerHTML = `
-        <img src="/src/public/beranda/man.png" alt="Foto Psikolog" id="psychologPhoto" />
-        <div class="info-riwayat">
-          <div class="info-text">
-            <p>
-              ${riwayat.psychologist_name}<br />
-              ${riwayat.schedule_date}<br />
-              ${riwayat.schedule_time}<br />
-              Via ${riwayat.type}
-            </p>
-          </div>
-          <div class="status-button">
-            <span class="status-riwayat">${riwayat.status}</span>
-            <button type="button" onclick="openUlasanPopup()">ISI ULASAN</button>
-          </div>
-        </div>
-      `;
-
-      containerRiwayat.appendChild(riwayatElement);
-    });
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to fetch data. Status: ' + response.status);
+    }
+    return response.json();
   })
-  .catch(error => console.error('Error:', error));
+  .then(data => {
+    if (data && Array.isArray(data)) {
+      if (data.length === 0) {
+        // Tidak ada data riwayat
+        const noDataElement = document.createElement('p');
+        noDataElement.textContent = 'Tidak ada riwayat konseling.';
+        containerRiwayat.appendChild(noDataElement);
+      } else {
+        // Menampilkan data riwayat
+        data.forEach(riwayat => {
+          const riwayatElement = document.createElement('div');
+          riwayatElement.classList.add('container-riwayat');
+
+          riwayatElement.innerHTML = `
+            <img src="/src/public/beranda/man.png" alt="Foto Psikolog" id="psychologPhoto" />
+            <div class="info-riwayat">
+              <div class="info-text">
+                <p>
+                  ${riwayat.psychologist_name}<br />
+                  ${riwayat.schedule_date}<br />
+                  ${riwayat.schedule_time}<br />
+                  Via ${riwayat.type}
+                </p>
+              </div>
+              <div class="status-button">
+                <span class="status-riwayat">${riwayat.status}</span>
+                <button type="button" onclick="openUlasanPopup()">ISI ULASAN</button>
+              </div>
+            </div>
+          `;
+
+          containerRiwayat.appendChild(riwayatElement);
+        });
+      }
+    } else {
+      console.error('Invalid data format received from the server.');
+    }
+  })
+  .catch(error => console.error('Error fetching data from API:', error));
