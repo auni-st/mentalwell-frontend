552a3aa6798d0472c60efa017389a7033a2302e0
Gibran Alfarabi <gibranalfarabi18@gmail.com>
2023-12-05 15:04:13 +0700

    update riwayat

--- src/scripts/views/pages/riwayat.js
+++ src/scripts/views/pages/riwayat.js
@@ -1,82 +1,38 @@
-const authToken = sessionStorage.getItem('authToken');
-const contentRiwayat = document.querySelector('.content-riwayat');
+const authToken = sessionStorage.getItem('authToken')
+const containerRiwayat = document.querySelector('.content-riwayat');
 
-try {
-  fetch('https://mentalwell-backend.vercel.app/history', {
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        console.error('Failed to fetch');
-        throw new Error('Failed to fetch data from the backend.');
-      }
-    })
-    .then(data => {
-      contentRiwayat.innerHTML = '';
+fetch('https://mentalwell-backend.vercel.app/history', {
+  method: 'GET',
+  headers: {
+    'Authorization': `Bearer ${authToken}`, 
+    'Content-Type': 'application/json'
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(riwayat => {
+      const riwayatElement = document.createElement('div');
+      riwayatElement.classList.add('container-riwayat');
 
-      data.forEach(history => {
-        const containerRiwayat = document.createElement('div');
-        containerRiwayat.classList.add('container-riwayat');
+      riwayatElement.innerHTML = `
+        <img src="/src/public/beranda/man.png" alt="Foto Psikolog" id="psychologPhoto" />
+        <div class="info-riwayat">
+          <div class="info-text">
+            <p>
+              ${riwayat.psychologist_name}<br />
+              ${riwayat.schedule_date}<br />
+              ${riwayat.schedule_time}<br />
+              Via ${riwayat.type}
+            </p>
+          </div>
+          <div class="status-button">
+            <span class="status-riwayat">${riwayat.status}</span>
+            <button type="button" onclick="openUlasanPopup()">ISI ULASAN</button>
+          </div>
+        </div>
+      `;
 
-        const psychologPhoto = document.createElement('img');
-        psychologPhoto.src = '/src/public/beranda/man.png';
-        psychologPhoto.alt = 'Foto Psikolog';
-
-        const infoRiwayat = document.createElement('div');
-        infoRiwayat.classList.add('info-riwayat');
-
-        const infoText = document.createElement('div');
-        infoText.classList.add('info-text');
-
-        const pInfo = document.createElement('p');
-        pInfo.innerHTML = `<strong>${history.psychologist_name}</strong><br />
-          ${history.schedule_date}<br />
-          ${history.schedule_time}<br />
-          Via ${history.type}`;
-
-        infoText.appendChild(pInfo);
-
-        const statusButton = document.createElement('div');
-        statusButton.classList.add('status-button');
-
-        const statusRiwayat = document.createElement('span');
-        statusRiwayat.classList.add('status-riwayat');
-        statusRiwayat.textContent = history.status;
-
-        const buttonRiwayat = document.createElement('button');
-        buttonRiwayat.type = 'button';
-        buttonRiwayat.textContent = history.status === 'Selesai' ? 'ISI ULASAN' : 'Belum Selesai';
-
-        statusButton.appendChild(statusRiwayat);
-        statusButton.appendChild(buttonRiwayat);
-
-        infoRiwayat.appendChild(infoText);
-        infoRiwayat.appendChild(statusButton);
-
-        containerRiwayat.appendChild(psychologPhoto);
-        containerRiwayat.appendChild(infoRiwayat);
-
-        contentRiwayat.appendChild(containerRiwayat);
-
-        buttonRiwayat.addEventListener('click', function () {
-          if (history.status === 'Selesai') {
-            // Implement logic to open ulasan popup or navigate to ulasan page
-            console.log('Redirect to ulasan page or open ulasan popup for counseling id:', history.id);
-          } else {
-            // Implement logic for handling counseling that is not yet completed
-            console.log('Handle counseling that is not yet completed for id:', history.id);
-          }
-        });
-      });
-    })
-    .catch(error => {
-      console.error('Error during data fetching:', error);
+      containerRiwayat.appendChild(riwayatElement);
     });
-} catch (error) {
-  console.error('Error during data fetching:', error);
-}
+  })
+  .catch(error => console.error('Error:', error));
