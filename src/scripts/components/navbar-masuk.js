4d019f0df061b66d41b56d94be7820b24898a7c9
Rahmanhakim7 <hakimrahman@students.amikom.ac.id>
2023-12-06 20:47:29 +0700

    update profil psikolog

--- src/scripts/components/navbar-masuk.js
+++ src/scripts/components/navbar-masuk.js
@@ -141,195 +141,127 @@ class NavBarLogin extends HTMLElement {
 
     // Define the HTML content for the component
     this.shadowRoot.innerHTML = `
-                <style>
-                /* Add your styling for the navbar here */
-                nav {
-                  position: fixed;
-                  top: 0;
-                  z-index: 99;
-                  width: 100%;
-                }
-                .navbar {
-                  width: 100%;
-                  height: 89px;
-                  display: flex;
-                  justify-content: space-between;
-                  position: sticky;
-                  top: 0;
-                  z-index: 99;
-                  background-color: #fff;
-                }
+            <style>
+            /* Add your styling for the navbar here */
+            nav {
+              position: fixed;
+              top: 0;
+              z-index: 99;
+              width: 100%;
+            }
+            .navbar {
+              width: 100%;
+              height: 89px;
+              display: flex;
+              justify-content: space-between;
+              z-index: 99;
+              background-color: white;
+            }
 
-                .navbar .navbar-brand {
-                  display: flex;
-                  flex-direction: row;
-                  align-items: center;
-                  justify-content: space-between;
-                  margin-left: 2rem;
-                }
+            .navbar .navbar-brand {
+              display: flex;
+              flex-direction: row;
+              align-items: center;
+              justify-content: space-between;
+              margin-left: 2rem;
+            }
 
-                .navbar .navbar-brand h1 {
-                  color: #044b77;
-                  font-size: 25px;
-                  /* text-transform: uppercase; */
-                  margin-left: 5px;
-                }
+            .navbar .navbar-brand h1 {
+              color: #044b77;
+              font-size: 25px;
+              margin-left: 5px;
+              /* text-transform: uppercase; */
+            }
 
-                .navbar .navbar-list {
-                  display: flex;
-                  gap: 2rem;
-                  justify-content: center;
-                }
+            .navbar .navbar-list {
+              display: flex;
+              gap: 2rem;
+              justify-content: center;
+            }
 
-                .navbar .navbar-list .button {
-                  display: flex;
-                  align-items: center;
-                  margin-right: 2rem;
-                }
+            .navbar .navbar-list .button {
+              display: flex;
+              flex-direction: row;
+              align-items: center;
+              margin-right: 20px;
+            }
 
-                .navbar .navbar-list .button button {
-                  width: 95px;
-                  height: 44px;
-                  border-radius: 10px;
-                  background-color: #044b77;
-                  color: #ffffff;
-                  font-size: 17px;
-                }
+            .navbar .navbar-list .button button {
+              width: 95px;
+              height: 44px;
+              border-radius: 10px;
+              background-color: white;
+              color: black;
+              font-size: 17px;
+              border: none;
+            }
 
-                .navbar .navbar-list .button button:hover {
-                  background-color: #3ad0ea;
-                  color: #ffffff;
-                  background-color: #02bbdd;
-                  cursor: pointer;
-                  color: white;
-                }
+            .navbar .navbar-list .button button:hover {
+              color: #ffffff;
+              background-color: #02bbdd;
+              cursor: pointer;
+              color: white;
+            }
 
-                .navbar .navbar-list .navbar-list-item {
-                  text-decoration: none;
-                  display: flex;
-                  align-items: center;
-                  gap: 2rem;
-                }
+            .navbar .navbar-list .navbar-list-item {
+              text-decoration: none;
+              display: flex;
+              align-items: center;
+              gap: 2rem;
+            }
 
-                .navbar .navbar-list .list-item {
-                  list-style: none;
-                }
+            .navbar .navbar-list .list-item {
+              list-style: none;
+            }
 
-                .navbar .navbar-list .navbar-list-item .list-item a {
-                  text-decoration: none;
-                  list-style: none;
-                  color: black;
-                  font-size: 17px;
-                  font-weight: 500px;
-                  padding-top: 10px;
-                  padding-bottom: 10px;
-                }
+            .navbar .navbar-list .navbar-list-item .list-item a {
+              text-decoration: none;
+              list-style: none;
+              color: black;
+              font-size: 17px;
+              font-weight: 500px;
+              padding-top: 10px;
+              padding-bottom: 10px;
+            }
 
-                /* animasi */
-                .navbar .navbar-list .navbar-list-item .list-item a::after {
-                  content: "";
-                  width: 0;
-                  height: 2px;
-                  background-color: #000;
-                  display: block;
-                  transition: 0.2s linear;
-                }
+            /* animasi */
+            .navbar .navbar-list .navbar-list-item .list-item a::after {
+              content: "";
+              width: 0;
+              height: 2px;
+              background-color: #000;
+              display: block;
+              transition: 0.2s linear;
+            }
 
-                .navbar .navbar-list .navbar-list-item .list-item a:hover::after {
-                  width: 100%;
-                }
-
-                .navbar .navbar-list .button-user {
-                    display: flex;
-                    align-items: center;
-                  }
-                  
-                  .navbar .navbar-list .button-user button {
-                    width: 95px;
-                    height: 40px;
-                    background-color: white;
-                    border: none;
-                    font-size: 20px;
-                    cursor: pointer;
-                  }
-
-                #photoUser {
-                    border-radius: 50%;
-                    width: 50px;
-                    height: 50px;
-                    cursor: pointer;
-                  }
-
-                  // dropdown menu
-                  .button-user {
-                    position: relative;
-                    display: inline-block;
-                }
-                
-                .dropdown-content {
-                    display: none;
-                    position: absolute;
-                    background-color: #f9f9f9;
-                    min-width: 160px;
-                    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-                    z-index: 1;
-                    top: 90px;
-                    right: 0;
-                    border-bottom-left-radius: 10px;
-                }
-                
-                .dropdown-content a {
-                    color: black;
-                    padding: 12px 16px;
-                    display: block;
-                    text-decoration: none;
-                }
-                
-                .dropdown-content a:hover {
-                    background-color: #044B77;
-                    color: #fff;
-                }
-
-                .keluar {
-                  border-bottom-left-radius: 10px;
-                }
-              </style>
-              
-              <nav>
-                  <div class="navbar">
-                      <div class="navbar-brand">
-                      <img src="/src/public/logo/logo-mentalwell.jpg" width="50px" alt="logo mentalwell">
-                          <h1>MentalWell</h1>
-                          <img src="/src/public/logo/2.png">
-                      </div>
-                      <div class="navbar-list">
-                          <div class="button-user" id="userDropdown">
-                              <img src="/src/public/beranda/man.png" alt="Foto User" id="photoUser" >
-                              <button type="submit">John Doe</button>
-                              <div class="dropdown-content">
-                                  <a href="#">Profil saya</a>
-                                  <a class="keluar" href="#">Keluar</a>
-                              </div>
-                          </div>
-                      <div>
+            .navbar .navbar-list .navbar-list-item .list-item a:hover::after {
+              width: 100%;
+            }
+          </style>
+          
+          <nav>
+              <div class="navbar">
+                  <div class="navbar-brand">
+                      <h1>MentallWell</h1>
+                      <img src="/src/public/logo/logo-mentalwell.jpg" alt="imagelogo" width="50px" height="50px">
                   </div>
-              </nav>
+                  <div class="navbar-list">
+                      <ul class="navbar-list-item">
+                          <li class="list-item"><a href="./">Beranda</a></li>
+                          <li class="list-item"><a href="/artikel">Artikel</a></li>
+                          <li class="list-item"><a href="/listpsikolog">Cari Psikolog</a></li>
+                          <li class="list-item"><a href="/tentangkami">Tentang Kami</a></li>
+                      </ul>
+                      <div class="button">
+                          <img src="/src/public/beranda/man.png" alt="Foto User" id="photoUser" width="60px" height="60px">
+                          <h2>John Doe</h2>
+                      </div>
+                  </div>
+              </div>
+          </nav>
       `;
-    // Get userDropdown element within Shadow DOM
-    const userDropdown = this.shadowRoot.getElementById("userDropdown");
-
-    // Add event listeners for mouseover and mouseout within Shadow DOM
-    userDropdown.addEventListener("mouseover", () => {
-      userDropdown.querySelector(".dropdown-content").style.display = "block";
-    });
-
-    userDropdown.addEventListener("mouseout", () => {
-      userDropdown.querySelector(".dropdown-content").style.display = "none";
-    });
   }
 }
-
-
 
 const authToken = sessionStorage.getItem('authToken');
 
