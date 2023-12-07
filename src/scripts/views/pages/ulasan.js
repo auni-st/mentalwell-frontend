d2917c9ec162f3a6d2104f4dcc12b3ddbe4ca8eb
Gibran Alfarabi <gibranalfarabi18@gmail.com>
2023-12-08 03:11:38 +0700

    update

--- src/scripts/views/pages/ulasan.js
+++ src/scripts/views/pages/ulasan.js
@@ -1,3 +1,5 @@
+const authToken = sessionStorage.getItem('authToken');
+
 function openUlasanPopup() {
   document.getElementById('container-ulasan').style.display = 'flex';
   document.getElementById('overlay').style.display = 'block';
@@ -18,7 +20,37 @@ function closeUlasanPopup() {
 }
 
 function authenticate(event) {
-  event.preventDefault(); // Prevent the default form submission for this example
-  // Add your authentication logic here
+  event.preventDefault(); 
   alert('Authentication logic goes here!');
 }
+
+function submitUlasan(ulasan) {
+  fetch('https://mentalwell-backend.vercel.app/history/counselings/38', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${authToken}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ review: ulasan })
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to submit review. Status: ' + response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Review submitted successfully:', data);
+    })
+    .catch(error => console.error('Error submitting review:', error))
+    .finally(() => {
+      closeUlasanPopup();
+    });
+}
+
+document.querySelector('.content-ulasan').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const ulasan = document.getElementById('ulasan').value;
+
+  submitUlasan(ulasan);
+});
