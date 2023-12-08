5631c40e8a6811d63fd0e02b8378d9a6db42ec6b
Gibran Alfarabi <gibranalfarabi18@gmail.com>
2023-12-08 15:39:49 +0700

    update edit profil pasien

--- src/scripts/views/pages/editprofilpasien.js
+++ src/scripts/views/pages/editprofilpasien.js
@@ -1,4 +1,5 @@
 const authToken = sessionStorage.getItem('authToken');
+const editIcon = document.getElementById('editIcon');
 
 function getPatientProfile() {
   fetch('https://mentalwell-backend.vercel.app/patient', {
@@ -21,21 +22,40 @@ function getPatientProfile() {
 }
 
 function displayPatientProfile(patientData) {
-  document.querySelector('.editpasien-container h3').innerText = patientData.users.name;
-  document.querySelector('.editpasien-form #email h4').innerText = patientData.users.email;
-  document.getElementById('namapanggilan').value = patientData.users.nickname;
-  document.getElementById('nowa').value = patientData.users.phone_number;
-  document.getElementById('tgllahir').value = patientData.users.birthdate;
-  document.getElementById('gender').value = patientData.users.gender;
+  document.querySelector('.editpasien-container h3').innerText = patientData.full_name;
+  document.querySelector('.editpasien-form #email h4').innerText = patientData.email;
+  document.getElementById('namalengkap').value = patientData.full_name;
+  document.getElementById('namapanggilan').value = patientData.nickname;
+  document.getElementById('nowa').value = patientData.phone_number;
+  document.getElementById('tgllahir').value = patientData.birthdate;
+  document.getElementById('gender').value = patientData.gender;
+
+  document.getElementById('namalengkap').readOnly = true;
+  document.getElementById('email').readOnly = true;
+
+  editIcon.addEventListener('click', function () {
+    document.getElementById('namalengkap').readOnly = false;
+    document.getElementById('namapanggilan').readOnly = false;
+    document.getElementById('nowa').readOnly = false;
+    document.getElementById('tgllahir').readOnly = false;
+    document.getElementById('gender').readOnly = false;
+  });
 }
 
 function editPatientProfile(formData) {
   fetch('https://mentalwell-backend.vercel.app/patient', {
     method: 'PUT',
     headers: {
-      'Authorization': `Bearer ${authToken}`
+      'Authorization': `Bearer ${authToken}`,
+      'Content-Type': 'application/json'
     },
-    body: formData
+    body: JSON.stringify({
+      full_name: formData.get('namalengkap'),
+      nickname: formData.get('namapanggilan'),
+      phone_number: formData.get('nowa'),
+      birthdate: formData.get('tgllahir'),
+      gender: formData.get('gender')
+    })
   })
     .then(response => {
       if (!response.ok) {
@@ -45,22 +65,23 @@ function editPatientProfile(formData) {
     })
     .then(data => {
       console.log('Patient profile edited successfully:', data);
+      displayPatientProfile(data);
     })
     .catch(error => console.error('Error editing patient profile:', error));
 }
 
 getPatientProfile();
 
-const editIcon = document.querySelector('.fa-edit');
-editIcon.addEventListener('click', function () {
-  document.getElementById('namapanggilan').readOnly = false;
-  document.getElementById('nowa').readOnly = false;
-});
-
 document.querySelector('.editpasien-form').addEventListener('submit', function (event) {
   event.preventDefault();
 
   const formData = new FormData(this);
+
+  document.getElementById('namalengkap').readOnly = true;
+  document.getElementById('namapanggilan').readOnly = true;
+  document.getElementById('nowa').readOnly = true;
+  document.getElementById('tgllahir').readOnly = true;
+  document.getElementById('gender').readOnly = true;
 
   editPatientProfile(formData);
 });
