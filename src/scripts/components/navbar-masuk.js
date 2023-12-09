398221dd126eb120bb5517f2d8e7db7b576751d7
Rahmanhakim7 <hakimrahman@students.amikom.ac.id>
2023-12-09 19:47:45 +0700

    update button daftar konseling sebelum masuk dan sesudah masuk

--- src/scripts/components/navbar-masuk.js
+++ src/scripts/components/navbar-masuk.js
@@ -310,7 +310,7 @@ class NavBarLogin extends HTMLElement {
     profilLink.addEventListener('click', () => {
       const urlParams = new URLSearchParams(window.location.search);
       const pasienId = urlParams.get('id');
-      window.location.href = `https://mentalwell-backend.vercel.app/patient`;
+      window.location.href = `https://mentalwell.vercel.app/editprofilpasien`;
     });
 
     this.shadowRoot.querySelector('.keluar').addEventListener('click', () => {
