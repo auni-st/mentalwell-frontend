a444aadc312484d40273ffad6f1db06fb82eb9bf
rizkiardii <rizkiseptiardi95@gmail.com>
2023-12-09 12:31:01 +0700

    menyelesaikan bug ubahsandi

--- src/scripts/views/pages/ubahsandi.js
+++ src/scripts/views/pages/ubahsandi.js
@@ -1,23 +1,23 @@
 const urlParams = new URLSearchParams(window.location.search);
-const token = urlParams.get('token');
+const token = urlParams.get("token");
 
 if (!token) {
   console.error(`Token doesn't exist`);
   Swal.fire({
-    title: 'Gagal!',
-    text: 'Tautan Ubah Kata Sandi Tidak Valid',
-    icon: 'error',
+    title: "Gagal!",
+    text: "Tautan Ubah Kata Sandi Tidak Valid",
+    icon: "error",
     showConfirmButton: true,
   });
 }
 
-const ubahsandiForm = document.getElementById('ubahsandi-form');
+const ubahsandiForm = document.getElementById("ubahsandi-form");
 
-ubahsandiForm.addEventListener('submit', async (event) => {
+ubahsandiForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
-  const newPassword = document.getElementById('sandibaru').value;
-  const newPasswordConfirmation = document.getElementById('konfirmasi').value;
+  const newPassword = document.getElementById("sandibaru").value;
+  const newPasswordConfirmation = document.getElementById("konfirmasi").value;
 
   const formData = {
     newPassword,
@@ -25,36 +25,38 @@ ubahsandiForm.addEventListener('submit', async (event) => {
   };
 
   try {
-    const response = await fetch(`https://mentalwell-backend.vercel.app/reset-password/${token}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
+    const response = await fetch(
+      `https://mentalwell-backend.vercel.app/reset-password/${token}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    );
 
     if (response.ok) {
       Swal.fire({
-        title: 'Ubah Sandi Berhasil!',
-        text: 'Anda Bisa Masuk Sekarang',
-        icon: 'success',
+        title: "Ubah Sandi Berhasil!",
+        text: "Anda Bisa Masuk Sekarang",
+        icon: "success",
         showConfirmButton: true,
       });
 
-      window.location.href = 'https://mentalwell.vercel.app/'
-      
+      window.location.href = "https://mentalwell.vercel.app/";
     } else {
       const responseData = await response.json();
-      const errorMessage = responseData.message || 'Error occured';
+      const errorMessage = responseData.message || "Error occured";
       Swal.fire({
-        title: 'Error!',
+        title: "Error!",
         text: `Ubah Sandi Gagal!`,
-        icon: 'error',
+        icon: "error",
         showConfirmButton: true,
-      });    }
-
+      });
+    }
   } catch (error) {
-    console.error('Password reset error:', error);
-    alert('Error during password reset. Try again')
+    console.error("Password reset error:", error);
+    alert("Error during password reset. Try again");
   }
-})
+});
