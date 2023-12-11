741e3e39e076a4dbd7317d98ef97a66fb6ee3f73
Gibran Alfarabi <gibranalfarabi18@gmail.com>
2023-12-11 18:56:11 +0700

    update profil psikolog

--- src/scripts/views/pages/editprofilpsikolog.js
+++ src/scripts/views/pages/editprofilpsikolog.js
@@ -1,70 +1,90 @@
-const authToken = sessionStorage.getItem('authToken');
+const token = sessionStorage.getItem('authToken');
+const form = document.querySelector('.editpsikolog-form');
 
-function getPsychologistProfile() {
-  fetch('https://mentalwell-backend.vercel.app/psychologist/profile', {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${authToken}`,
-      'Content-Type': 'application/json'
+document.addEventListener('DOMContentLoaded', async function () {
+  try {
+    const response = await fetch('https://mentalwell-backend.vercel.app/psychologist/profile', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch psychologist data. Status: ${response.status}`);
     }
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to fetch data. Status: ' + response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      displayPsychologistProfile(data);
-    })
-    .catch(error => console.error('Error fetching psychologist profile:', error));
-}
 
-function displayPsychologistProfile(psychologistData) {
-  document.getElementById('namapanggilan').innerText = psychologistData.name;  
-  document.getElementById('email').innerText = psychologistData.email;  
-  document.getElementById('nowa').value = psychologistData.phone_number;
-  document.getElementById('tgllahir').value = psychologistData.birthdate;
-  document.getElementById('gender').value = psychologistData.gender;
-  document.getElementById('bio').value = psychologistData.bio;
-  document.getElementById('topik').value = psychologistData.psychologists_topic;
-  document.getElementById('pengalaman').value = psychologistData.experience;
-}
+    const psychologistData = await response.json();
 
-function editPsychologistProfile(formData) {
-  fetch('https://mentalwell-backend.vercel.app/psychologist', {
-    method: 'PUT',
-    headers: {
-      'Authorization': `Bearer ${authToken}`
-    },
-    body: formData
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to edit psychologist profile. Status: ' + response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Psychologist profile edited successfully:', data);
-    })
-    .catch(error => console.error('Error editing psychologist profile:', error));
-}
+    // Update email
+    document.getElementById('email').innerHTML = `<h4>${psychologistData.users.email}</h4>`;
 
-getPsychologistProfile();
+    // Update name
+    document.getElementById('namapanggilan').value = psychologistData.users.name;
 
-const editIcon = document.querySelector('.fa-edit');
-editIcon.addEventListener('click', function () {
-  document.getElementById('namapanggilan').contentEditable = true;  
-  document.getElementById('nowa').readOnly = false;
-  document.getElementById('bio').readOnly = false;
-  document.getElementById('topik').readOnly = false;
+    // Update phone number
+    document.getElementById('nowa').value = psychologistData.users.phone_number;
+
+    // Update birthdate
+    document.getElementById('tgllahir').value = psychologistData.users.birthdate;
+
+    // Update gender
+    document.getElementById('gender').value = psychologistData.users.gender;
+
+    // Update bio
+    document.getElementById('bio').value = psychologistData.bio;
+
+    // Update experience
+    document.getElementById('pengalaman').value = psychologistData.experience;
+
+    // Update topics
+    psychologistData.psychologists_topics.forEach((topic, index) => {
+      document.getElementById(`topik${index + 1}`).checked = true;
+    });
+  } catch (error) {
+    console.error('Error fetching psychologist data:', error);
+  }
 });
 
-document.querySelector('.editpsikolog-form').addEventListener('submit', function (event) {
+form.addEventListener('submit', async function (event) {
   event.preventDefault();
+  const newName = document.getElementById('namapanggilan').value;
+  const newPhone_number = document.getElementById('nowa').value;
+  const newBirthdate = document.getElementById('tgllahir').value;
+  const newGender = document.getElementById('gender').value;
+  const newBio = document.getElementById('bio').value;
+  const newExperience = document.getElementById('pengalaman').value;
 
-  const formData = new FormData(this);
+  const newTopics = [];
+  const checkboxes = document.querySelectorAll('input[name="topik"]:checked');
+  checkboxes.forEach((checkbox) => {
+    newTopics.push(checkbox.value);
+  });
 
-  editPsychologistProfile(formData);
+  const formData = new FormData();
+  formData.append('newName', newName);
+  formData.append('newPhone_number', newPhone_number);
+  formData.append('newBirthdate', newBirthdate);
+  formData.append('newGender', newGender);
+  formData.append('newTopics[]', newTopics.join(','));
+  formData.append('newBio', newBio);
+  formData.append('newExperience', newExperience);
+
+  try {
+    const response = await fetch('https://mentalwell-backend.vercel.app/psychologist', {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+      body: formData,
+    });
+
+    if (response.ok) {
+      console.log(response);
+    } else {
+      const errorMessage = await response.text();
+      alert(`Failed to update profile. Error: ${errorMessage}`);
+    }
+  } catch (error) {
+    console.error('Error updating psychologist profile:', error);
+  }
 });
