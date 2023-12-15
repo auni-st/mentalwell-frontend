60d19e657e383e982a97aba38e7f43fc1d678f16
Rahmanhakim7 <hakimrahman@students.amikom.ac.id>
2023-12-15 20:00:37 +0700

    update

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import navbar from "./static-components/navbar.html";
-import footer from "./static-components/footer.html";
-document.onload = function () {
-  document.getElementById("root").innerHTML = navbar + footer;
-};
